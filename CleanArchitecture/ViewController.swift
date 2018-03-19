ab0378bc1826fc751df3f00bdd9a28014bf2963f
amerexia <dbelannab@gmail.com>
2018-03-19 14:55:48 +0000

    Initial Commit

---
+++ CleanArchitecture/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CleanArchitecture
+//
+//  Created by Dounia BELANNAB on 19/03/2018.
+//  Copyright © 2018 Dounia BELANNAB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
